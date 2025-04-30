0701ea59ac65862cacde5919e4a48136cf9519ad
theoarruda2 <theoarruda8@gmail.com>
2025-04-30 11:42:26 -0300

    commit aula codigo antonio do dia 30/04/25

--- apps/settings.py
+++
@@ -1,2 +0,0 @@
-APPS_DIR = os.path.join(BASE_DIR, 'apps') 
-sys.path.insert(0, APPS_DIR) 
--- modeloapp/urls.py
+++ modeloapp/urls.py
@@ -14,8 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls.static import static
-from django.conf import settings
+
 
 urlpatterns = [
   path('admin/',admin.site.urls),
